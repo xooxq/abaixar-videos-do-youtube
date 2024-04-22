45b0d237c98942d0ce7ca405168750284b84f739
xooxq <152539555+xooxq@users.noreply.github.com>
2024-04-21 22:58:51 -0300

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ while True:
     while True:
         system('cls')
         try:
+            #caso você digite errado o link ou qualquer coisa que não seja o link, vai retornar um erro do pytube e não do código em sí.
             x = input("Digite a url do link (0 para cancelar): ").strip()
             if x == "0":
                 break
@@ -30,8 +31,8 @@ while True:
             y = int(input("""[0] Cancelar
 [1] Vídeo
 [2] Áudio
-"""))       
-            if y<=2 and y>=0:
+"""))
+            if y <= 2 and y >= 0:
                 break
             else:
                 limpar("Digite apenas de 0 a 2!")
@@ -42,6 +43,7 @@ while True:
     print("-"*30)
 
     if y == 1:
+        #caso você queira abaixar outra resolução, só alterar de "720p", para a resolução desejada.
         vd = yt.streams.filter(res="720p", progressive=True, file_extension="mp4").first()
         if vd:
             vd.download(filename=f"{yt.title}.mp4")
@@ -54,7 +56,8 @@ while True:
         else:
             print('Infelizmente não é possível abaixar esse áudio.')
     sleep(2)
-    z = input("Deseja abaixar mais coisas (s/n) ?: ").strip().upper()
+    z = input("Deseja abaixar mais algo (s/n) ?: ").strip().upper()
+    #e aqui, fazendo a verificação para que o user digite apenas "s" ou "n".
     while z not in "SN":
         limpar("Digite apenas 's' ou 'n'.")
         z = input("Deseja abaixar mais coisas (s/n) ?: ").strip().upper()
@@ -62,3 +65,4 @@ while True:
         system('cls')
         print('Fim do programa. Volte logo\n')
         break
+    #faça bom uso do código :)
